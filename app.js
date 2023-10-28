9b7b2aac2f3ebb1b3126d991e74f73a08bf56b8b
Isaac Turner <spacey_sooty@outlook.com>
2023-10-28 21:44:37 +0800

    add fade away for title on click

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let main = document.getElementById("body");
+
 let title = document.createElement("h1");
 title.id = "title";
 title.innerHTML = "Hello World!";
 
-let main = document.getElementById("body");
-main.appendChild(title);
+title.addEventListener('click', async () => {
+  title.animate(
+    [
+      {
+        // from
+        opacity: 1,
+        color: "#000",
+      },
+      {
+        // to
+        opacity: 0,
+        color: "#fff",
+      },
+    ],
+    2000,
+  );
+  await sleep(2000).then(() => {
+    title.remove()
+  });
+});
 
+main.appendChild(title);
